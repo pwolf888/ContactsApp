7150bdcb672d19330b749bd9986d6dbad5af3b09
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-08-03 01:10:09 +1000

    Added in the list view
    Learned the code here:
    http://www.androidinterview.com/android-custom-listview-with-image-and-t
    ext-using-arrayadapter/
    
    Will modify it to better suit my project.

--- ContactsApp/app/src/main/java/com/example/jonathanturnbull/contactsapp_jmturnbu/contactListPage.java
+++ ContactsApp/app/src/main/java/com/example/jonathanturnbull/contactsapp_jmturnbu/contactListPage.java
@@ -1,16 +1,36 @@
 package com.example.jonathanturnbull.contactsapp_jmturnbu;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class contactListPage extends AppCompatActivity {
+public class contactListPage extends ListActivity {
+
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War",
+            "Jeff",
+            "Dingo Jones",
+            "Captain Crunch",
+            "Jora"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list_page);
+        this.setListAdapter(new ArrayAdapter<String>(
+                this, R.layout.contactlist,
+                R.id.Itemname,itemname));
     }
 
     public void AddContact(View view) {
@@ -22,4 +42,7 @@ public class contactListPage extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
+
 }
