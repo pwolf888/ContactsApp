39e3370babce5b9eed3362889186955586f4fa48
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-08-02 23:05:03 +1000

    Fixing up my way of creating my layouts

--- ContactsApp/app/src/main/java/com/example/jonathanturnbull/contactsapp_jmturnbu/addContactsPage.java
+++ ContactsApp/app/src/main/java/com/example/jonathanturnbull/contactsapp_jmturnbu/addContactsPage.java
@@ -2,23 +2,12 @@ package com.example.jonathanturnbull.contactsapp_jmturnbu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-
-
-
+public class addContactsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_add_contacts_page);
     }
 }
-
